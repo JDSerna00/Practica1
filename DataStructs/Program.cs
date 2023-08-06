8706704f01b0eaff816dae43f8186df2e7101c1e
JDSerna00 <83099671+JDSerna00@users.noreply.github.com>
2023-08-06 09:16:48 -0500

    VS test project

---
+++ DataStructs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
